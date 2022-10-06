84dc93e093ae43398c786406380334e65886f5e4
junnel308 <junnelpadilla08@gmail.com>
2022-10-06 15:21:32 -0700

    added a function where the button is clicked, the question will show up

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,10 @@
 var startButton = document.querySelector("#start");
 var timeDisplay =document.querySelector('span');
-
-let timeSeconds = 3;
+var incorrectAnswer = document.querySelector('#chosen-answer');
+var questionOne = document.querySelector("#first-question");
+var questionTwo = document.querySelector("#second-question");
+var questionThree = document.querySelector("#third-question");
+let timeSeconds = 90;
 
 
 function displayTimer() {
@@ -11,18 +14,31 @@ function displayTimer() {
     timerText.value = timerDown;
 }
 
+function displayQuestions(){
+
+}
+
+startButton.addEventListener("click", displayTimer);
+
 function countdownTimer () {
 
     var countDown = setInterval(() => {
         timeSeconds--;
         timeDisplay.innerHTML = `${timeSeconds}`;
-
+        
         if(timeSeconds <= 0) {
             clearInterval(countDown);
             alert("Times Up! Please Try Again!");
         }
-    },1000);
+    },1000); 
+
+    startButton.style.visibility= "hidden";
+    questionOne.style.visibility = "visible";
+
+    incorrectAnswer.addEventListener("click", () => {
+        alert("Incorrect! Please Try Again!");
+        questionOne.style.visibility = "hidden";
+        questionTwo.style.visibility = "visible";
+    });
 
 }
-
-startButton.addEventListener("click", displayTimer);
